eb740a39a32dc9851e14c2d83e5520ed378ff2ca
Yurij Kysylov <87658578+YuggOgg@users.noreply.github.com>
2024-02-18 21:09:24 +0200

    Update Main.java

--- Lab_1/Work_1/Main.java
+++ Lab_1/Work_1/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static int[] numbers = new int[10];
     static int minRange = 1, maxRange = 10;
